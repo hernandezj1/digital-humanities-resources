cbd7d68048a614cf9ee1b1c241d57546e44c78af
Jose Hernandez <98195541+hernandezj1@users.noreply.github.com>
2025-02-12 16:49:23 -0500

    Create data.js
    data file same as json

---
+++ assets/js/data.js
@@ -0,0 +1,23 @@
+const tutorialsData = [
+    {
+        "id": 1,
+        "title": "Introduction to Digital Humanities",
+        "topics": ["History", "Technology"],
+        "mode": ["Tutorial"],
+        "link": "https://example.com/dh-intro"
+    },
+    {
+        "id": 2,
+        "title": "Text Mining Basics",
+        "topics": ["Linguistics", "Data Science"],
+        "mode": ["Docs"],
+        "link": "https://example.com/text-mining"
+    },
+    {
+        "id": 3,
+        "title": "Digital Archives",
+        "topics": ["History", "Archives"],
+        "mode": ["Video"],
+        "link": "https://example.com/digital-archives"
+    }
+];
